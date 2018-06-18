d17f6a15694ab87bbd015876ca4f2a108e99c65d
John Forster <j_forster@hotmail.co.uk>
2018-06-18 16:15:13 +0100

    Chapter 8 complete. Touching in and out implemented. #in_journey? implemented.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,9 +3,14 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
+
+  def in_journey?
+    in_journey
+  end
 
   def top_up(amount)
     raise "Balance cannot exceed £#{MAX_BALANCE}." if balance + amount > MAX_BALANCE
@@ -14,5 +19,13 @@ class Oystercard
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,19 +10,38 @@ describe Oystercard do
 
   describe '#top_up' do
     it 'should increase balance by given amount' do
-      expect{ oystercard.top_up(3) }.to change{ oystercard.balance }.by(3)
+      expect { oystercard.top_up(3) }.to change { oystercard.balance }.by(3)
     end
 
     it 'should raise an error if trying to go above max balance' do
       error1 = "Balance cannot exceed £#{Oystercard::MAX_BALANCE}."
       excess = Oystercard::MAX_BALANCE + 1
-      expect{oystercard.top_up(excess)}.to raise_error(error1)
+      expect { oystercard.top_up(excess) }.to raise_error(error1)
     end
   end
 
   describe '#deduct' do
     it 'should decrease balance by given amount' do
-      expect{ oystercard.deduct(3) }.to change{ oystercard.balance }.by(-3)
+      expect { oystercard.deduct(3) }.to change { oystercard.balance }.by(-3)
+    end
+  end
+
+  describe '#in_journey?' do
+    it { is_expected.to respond_to(:in_journey?) }
+  end
+
+  describe '#touch_in' do
+    it 'should set in_journey to true' do
+      oystercard.touch_in
+      expect(oystercard).to be_in_journey
+    end
+  end
+
+  describe '#touch_out' do
+    it 'should set in_journey to false' do
+      oystercard.instance_variable_set(:@in_journey, true)
+      oystercard.touch_out
+      expect(oystercard).not_to be_in_journey
     end
   end
 end
