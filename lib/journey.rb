0afd18c3cc503d2b5ebcd2bd3292592730856be1
John Forster <j_forster@hotmail.co.uk>
2018-06-20 15:20:50 +0100

    Challenge 14 complete. #fare, #complete, and #in_progress? added to Journey class.

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,10 +1,25 @@
 # Creates and stores information about journey
 class Journey
   MINIMUM_FARE = 1
+  PENALTY_FARE = 6
 
   def initialize(entry_station = nil)
     @entry_station = entry_station
+    @in_progress = true
   end
 
-  attr_reader :entry_station
+  attr_reader :entry_station, :exit_station
+
+  def fare
+    @entry_station && @exit_station ? MINIMUM_FARE : PENALTY_FARE
+  end
+
+  def complete(exit_station)
+    @exit_station = exit_station
+    @in_progress = false
+  end
+
+  def in_progress?
+    @in_progress
+  end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,11 +2,35 @@ require 'journey'
 
 describe Journey do
   it { is_expected.to respond_to(:entry_station) }
-
+  let(:journey) { Journey.new('Shoreditch') }
+  
   describe '#initialize' do
     it 'should set the entry station' do
-      journey = Journey.new(:shoreditch)
-      expect(journey.entry_station).to eq :shoreditch
+      expect(journey.entry_station).to eq 'Shoreditch'
+    end
+  end
+
+  describe '#fare' do
+    it 'calculates the fare' do
+      journey.instance_variable_set(:@exit_station, 'Aldgate')
+      expect(journey.fare).to eq Journey::MINIMUM_FARE
+    end
+
+    it 'returns penalty fare if no entry station is specified' do
+      journey2 = Journey.new
+      expect(journey2.fare).to eq Journey::PENALTY_FARE
+    end
+  end
+
+  describe '#complete' do
+    it 'assigns the exit station' do
+      journey.complete('Aldgate')
+      expect(journey.exit_station).to eq 'Aldgate'
+    end
+
+    it 'completes the journey' do
+      journey.complete('Aldgate')
+      expect(journey.in_progress?).to eq false
     end
   end
 end
